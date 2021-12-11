f7a3a80efb58777b30d1891718f268a0a25617ce
Wiktor Wolski <witek119@gmail.com>
2021-12-11 21:54:41 +0000

    corrections

--- day05/solution.py
+++ day05/solution.py
@@ -18,15 +18,14 @@ def solutions(lines):
             points += [(x1, y) for y in range(min(y1, y2), max(y1, y2) + 1)]
         elif y1 == y2:
             points += [(x, y1) for x in range(min(x1, x2), max(x1, x2) + 1)]
-        else:
-            if y1 < y2 and x1 < x2:
-                diagonals += [(x, y1 + i) for i, x in enumerate(range(x1, x2 + 1))]
-            if y1 < y2 and x1 > x2:
-                diagonals += [(x, y2 - i) for i, x in enumerate(range(x2, x1 + 1))]
-            if y1 > y2 and x1 < x2:
-                diagonals += [(x, y1 - i) for i, x in enumerate(range(x1, x2 + 1))]
-            if y1 > y2 and x1 > x2:
-                diagonals += [(x, y2 + i) for i, x in enumerate(range(x2, x1 + 1))]
+        elif y1 < y2 and x1 < x2:
+            diagonals += [(x, y1 + i) for i, x in enumerate(range(x1, x2 + 1))]
+        elif y1 < y2 and x1 > x2:
+            diagonals += [(x, y2 - i) for i, x in enumerate(range(x2, x1 + 1))]
+        elif y1 > y2 and x1 < x2:
+            diagonals += [(x, y1 - i) for i, x in enumerate(range(x1, x2 + 1))]
+        elif y1 > y2 and x1 > x2:
+            diagonals += [(x, y2 + i) for i, x in enumerate(range(x2, x1 + 1))]
                 
     ### part one
     print(len([point for point, count in Counter(points).items() if count > 1]))
